336f9b7e973812afd9918be460965852decc716d
devbelek <belekasrarov10@gmail.com>
2024-08-20 18:19:04 +0600

    Создал файл render.yaml

--- Gimnasium/settings.py
+++ Gimnasium/settings.py
@@ -280,6 +280,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
+    "https://gimnasium.onrender.com",
 ]
 CORS_ALLOW_METHODS = [
     'GET',
