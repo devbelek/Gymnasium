4f40f6913e92af5104934b234359af4a6d732305
devbelek <belekasrarov10@gmail.com>
2024-08-20 17:05:26 +0600

    REDIS redeploy

--- Gimnasium/settings.py
+++ Gimnasium/settings.py
@@ -269,8 +269,8 @@ REST_FRAMEWORK = {
 SOCIALACCOUNT_ADAPTER = 'users.signals.MySocialAccountAdapter'
 ACCOUNT_ADAPTER = 'users.adapters.MyAccountAdapter'
 
-CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
-CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
